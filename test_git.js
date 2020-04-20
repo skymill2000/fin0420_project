039d8e6be32534027e4cb3e1c024e38113b58940
skymill2000 <skymill2000@gmail.com>
2020-04-20 16:33:02 +0900

    commit2

--- test_git.js
+++ test_git.js
@@ -1,9 +1,9 @@
 console.log('git Test');
 console.log('git Test');
+console.log('git ');
 console.log('git Test');
 console.log('git Test');
+console.log('git t');
 console.log('git Test');
-console.log('git Test');
-console.log('git Test');
-console.log('git Test');
+console.log('git t');
 console.log('git Test');
