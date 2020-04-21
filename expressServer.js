ce1ba662f37c4da4f29112bbd2091debaf80e9e1
skymill2000 <skymill2000@gmail.com>
2020-04-21 15:45:30 +0900

    ejs front-engine add

--- expressServer.js
+++ expressServer.js
@@ -1,1 +1,18 @@
 //#work4 npm 에서 express 모듈 임포트 후에 대표 샘플 코드 실행
+const express = require('express')
+const app = express()
+ 
+app.set('views', __dirname + '/views');
+app.set('view engine','ejs')
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.get('/test', function(req, res){
+    res.render('ejsPage');
+})
+
+
+ 
+app.listen(3000)
