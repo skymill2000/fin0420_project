484f9bc6491fd6e3381ef034cde3a8a57777907f
skymill2000 <skymill2000@gmail.com>
2020-04-21 16:34:34 +0900

    ejs design file add

--- expressServer.js
+++ expressServer.js
@@ -5,6 +5,8 @@ const app = express()
 app.set('views', __dirname + '/views');
 app.set('view engine','ejs')
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
