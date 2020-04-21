e89fc13ced204a3df28448afe343123686c880e6
skymill2000 <skymill2000@gmail.com>
2020-04-21 15:14:06 +0900

    expressServer.js add

--- httpServer.js
+++ httpServer.js
@@ -2,7 +2,7 @@ var http = require("http");
 http.createServer(function (req, res) {
     console.log('req!!!');
 	var body = "hello Server";
-	res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-	res.end("안녕하세요");
+	res.setHeader('Content-Type', 'text/html; charset=utf-8');
+	res.end("<html><h1>안녕하세요</h1></html>");
 }).listen(3000);
 console.log('sever is ready');
