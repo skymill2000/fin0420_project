44fe36029dce695e410ede349dd5df2eb47b0d3f
skymill2000 <skymill2000@gmail.com>
2020-04-22 11:16:17 +0900

    mysql connect test

---
+++ mysqlExample.js
@@ -0,0 +1,17 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '1q2w3e4r!',
+  database : 'fintech',
+  port : 3306
+});
+ 
+connection.connect();
+ 
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+ 
+connection.end();
