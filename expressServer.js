eae9e95c8d83686373cd354620e694598be5bae9
skymill2000 <skymill2000@gmail.com>
2020-04-22 11:33:48 +0900

    db connection in express server

--- expressServer.js
+++ expressServer.js
@@ -1,5 +1,7 @@
 //#work4 npm 에서 express 모듈 임포트 후에 대표 샘플 코드 실행
 const express = require('express')
+var mysql = require('mysql');
+
 const app = express()
  
 app.set('views', __dirname + '/views');
@@ -7,8 +9,17 @@ app.set('view engine','ejs')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 app.use(express.static(__dirname + '/public'));
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '1q2w3e4r!',
+  database : 'fintech',
+  port : 3306
+});
+ 
+connection.connect();
 
 app.get('/', function (req, res) {
   res.send('Hello World')
@@ -20,6 +31,10 @@ app.get('/test', function(req, res){
 
 app.post('/getData', function(req, res){
     var data = req.body.inputData;
+    connection.query('SELECT * FROM fintech.user', function (error, results, fields) {
+      if (error) throw error;
+      console.log('member list is : ', results);
+    });    
     console.log(data);
     console.log(req.body);
     res.json(1);
